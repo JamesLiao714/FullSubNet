ce643be23ecbaf6c37587220b27f21edc64a3852
JamesLiao714 <s9446760133@gmail.com>
2022-02-18 14:39:19 +0800

    test

--- test.py
+++ test.py
@@ -5,14 +5,9 @@ import shutil
 import numpy as np
 import config as cfg
 from models import DCCRN, CRN, FullSubNet  # you can import 'DCCRN' or 'CRN' or 'FullSubNet'
-from write_on_tensorboard import Writer
 from dataloader import create_dataloader
-from trainer import model_train, model_validate, \
-    model_perceptual_train, model_perceptual_validate, \
-    dccrn_direct_train, dccrn_direct_validate, \
-    crn_direct_train, crn_direct_validate, \
-    fullsubnet_test, model_test
-import soundfile as sf
+from trainer import  fullsubnet_test, model_test
+
 
 
  
@@ -39,6 +34,7 @@ def calculate_total_params(our_model):
 ###############################################################################
 # Set device
 DEVICE = torch.device(cfg.DEVICE)
+
 # Set model
 if cfg.model == 'DCCRN':
     model = DCCRN().to(DEVICE)
@@ -47,30 +43,22 @@ elif cfg.model == 'CRN':
 elif cfg.model == 'FullSubNet':
     model = FullSubNet().to(DEVICE)
 # Set optimizer and learning rate
-optimizer = torch.optim.Adam(model.parameters(), lr=cfg.learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr = cfg.learning_rate)
 total_params = calculate_total_params(model)
 
-# Set trainer and estimator
-if cfg.perceptual is True:
-    estimator = model_perceptual_validate
-elif cfg.model == 'FullSubNet':
+# Set estimator
+if cfg.model == 'FullSubNet':
     estimator = fullsubnet_test
-elif cfg.masking_mode == 'Direct(None make)' and cfg.model == 'DCCRN':
-    estimator = dccrn_direct_validate
-elif cfg.masking_mode == 'Direct(None make)' and cfg.model == 'CRN':
-    estimator = crn_direct_validate
 else:
-    estimator = model_test
-###############################################################################
-#                              Create Dataloader                              #
-###############################################################################
+    print('NO test module')
 
+# Create Dataloader     
 test_loader = create_dataloader(mode='test')
 
-###############################################################################
-#                        Set a log file to store progress.                    #
-#               Set a hps file to store hyper-parameters information.         #
-###############################################################################
+
+#  Set a log file to store progress.   
+#  Set a hps file to store hyper-parameters information.    
+
 print('Loading model from checkpoint: %s' % cfg.chkpt_model_test)
 
 # Set a log file to store progress.
@@ -81,20 +69,16 @@ model.load_state_dict(checkpoint['model'])
 optimizer.load_state_dict(checkpoint['optimizer'])
 epoch_start_idx = checkpoint['epoch'] + 1
 
-# if the loaded length is shorter than I expected, extend the length
-
 # make the file directory
 if not os.path.exists(cfg.result_path):
     os.mkdir(cfg.result_path)
 
-###############################################################################
-#                                    Test                                     #
-###############################################################################
+#  Test                                
 
 
 start_time = time.time()
 
-# Validation
+# test, generate result
 estimator(model, test_loader, DEVICE)
 
 print('Test Resport | takes {:.2f} seconds\n'
